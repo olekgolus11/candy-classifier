032336a201f2fa86ad3bacd9bf7b7a4d18405962
olekgolus11 <olekgolus02@gmail.com>
2023-12-17 17:40:30 +0100

    Add counter

--- CandyRecognizer.py
+++ CandyRecognizer.py
@@ -8,6 +8,7 @@ class CandyRecognizer:
     def __init__(self, video_path):
         self.video_path = video_path
         self.video_capture = cv2.VideoCapture(self.video_path)
+        self.counter = {"Red": 0, "Pink": 0, "Orange": 0, "Green": 0}
         if not self.video_capture.isOpened():
             raise Exception("Failed to open file")
         self.color_ranges = {
@@ -23,8 +24,10 @@ class CandyRecognizer:
             if not ret:
                 break  # Exit if no frame is loaded
             hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            self.find_middle_of_frame(frame)
             result_frame = self.detect_candy(frame, hsv_frame)
-            cv2.imshow('Detected Candies', result_frame)  # Display the result frame
+            frame_with_line = self.znajdz_linie_srodka_obrazu(result_frame)
+            cv2.imshow('Detected Candies', frame_with_line)  # Display the result frame
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break  # Exit if 'q' is pressed
         self.close_program()
@@ -40,7 +43,7 @@ class CandyRecognizer:
                 cv2.imshow(color, masks[color])
             candy_counts[color] = count
 
-        print("Ilość cukierków: " + str(candy_counts))
+        # print("Ilość cukierków: " + str(candy_counts))
         return result_frame  # Return the frame with drawn contours
 
     def detect_and_draw_contours(self, output_image, mask, color_name):
@@ -49,6 +52,10 @@ class CandyRecognizer:
         for contour in contours:
             if cv2.contourArea(contour) > 30000:  # Adjust the area threshold as needed
                 x, y, w, h = cv2.boundingRect(contour)
+                if x >= self.middle_x - 30 and x <= self.middle_x + 30:
+                    print(x)
+                    self.counter[color_name] += 1
+                    print(color_name + str(self.counter[color_name]))
                 cv2.rectangle(output_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(output_image, color_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 255, 0), 2)
                 count += 1
@@ -61,6 +68,27 @@ class CandyRecognizer:
     def close_program(self):
         self.video_capture.release()
         cv2.destroyAllWindows()
+
+    def find_middle_of_frame(self, frame):
+        self.height, self.width = frame.shape[:2]
+        self.middle_x = self.width // 2
+
+
+    # Funkcja do wyznaczania linii na środku
+    def znajdz_linie_srodka_obrazu(self, frame):
+        # Konwertuj do skali szarości
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Wykonaj progowanie
+        _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+
+        # Znajdź kontury
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Rysuj linię na środku
+        cv2.line(frame, (self.middle_x, 0), (self.middle_x, self.height), (0, 255, 0), 2)
+
+        return frame
 
 # Path to the video file
 video_path = 'videos/video1.mp4'
--- Colors.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Colors(Enum):
-    RED =[0, 0, 255]
-    PINK = [255, 0, 255]
-    ORANGE =[12, 232, 255]
-    GREEN = [0, 255, 96]
